2fe6ff625aa58f8c63a2db766f67aedfc665dd6e
Neo <mrrneo@outlook.com>
2026-06-07 07:41:48 +0600

    ...

--- movie_site/movie_app/serializers.py
+++ movie_site/movie_app/serializers.py
@@ -35,6 +35,7 @@ class UserSerializer(serializers.ModelSerializer):
             'refresh': str(refresh),
         }
 
+
 class LoginSerializer(serializers.Serializer):
     username = serializers.CharField()
     password = serializers.CharField(write_only=True)
@@ -56,86 +57,102 @@ class LoginSerializer(serializers.Serializer):
             'refresh': str(refresh),
         }
 
-# -----------------------------------------------------------------------
+
 class UserProfileSerializers(serializers.ModelSerializer):
     class Meta:
         model = UserProfile
         fields = '__all__'
+
 
 class UserProfileMovieDetailSerializers(serializers.ModelSerializer):
     class Meta:
         model = UserProfile
         fields = ['username']
 
+
 class CountrySerializers(serializers.ModelSerializer):
     class Meta:
         model = Country
         fields = ['country_name']
+
 
 class CountryDetailSerializers(serializers.ModelSerializer):
     class Meta:
         model = Country
         fields = ['country_name']
 
+
 class DirectorSerializers(serializers.ModelSerializer):
     class Meta:
         model = Director
         fields = ['director_name', 'bio', 'age', 'director_image']
+
 
 class DirectorMovieDetailSerializers(serializers.ModelSerializer):
     class Meta:
         model = Director
         fields = ['director_name']
 
+
 class ActorSerializers(serializers.ModelSerializer):
     class Meta:
         model = Actor
         fields = ['actor_name', 'bio', 'age', 'actor_image']
+
 
 class ActorMovieDetailSerializers(serializers.ModelSerializer):
     class Meta:
         model = Actor
         fields = ['actor_name']
 
+
 class GenreSerializers(serializers.ModelSerializer):
     class Meta:
         model = Genre
         fields = ['genre_name']
+
 
 class GenreDetailSerializers(serializers.ModelSerializer):
     class Meta:
         model = Genre
         fields = ['genre_name']
 
+
 class MovieSerializers(serializers.ModelSerializer):
-    year = serializers.DateField(format('%Y'))
+    year = serializers.DateField(format='%Y')
     genre = GenreSerializers(many=True)
     country = CountrySerializers()
+
     class Meta:
         model = Movie
         fields = ['id', 'movie_name', 'movie_image', 'year', 'genre', 'country', 'status_movie']
+
 
 class MovieMomentsSerializers(serializers.ModelSerializer):
     class Meta:
         model = MovieMoments
         fields = ['movie_moments']
 
+
 class MovieLanguagesSerializers(serializers.ModelSerializer):
     class Meta:
         model = MovieLanguages
         fields = ['movie_dubbing', 'video']
+
 
 class RatingSerializers(serializers.ModelSerializer):
     user = UserProfileMovieDetailSerializers()
     parent = UserProfileMovieDetailSerializers(source='parent.user', read_only=True, allow_null=True)
     created_date = serializers.DateTimeField(format='%d-%m-%Y %H:%M')
     check_comments = serializers.SerializerMethodField()
+
     class Meta:
         model = Rating
         fields = ['user', 'parent', 'text', 'stars', 'created_date', 'check_comments']
 
     def get_check_comments(self, obj):
         return model.predict(vector.transform([obj.text]))
+
 
 class MovieDetailSerializers(serializers.ModelSerializer):
     director = DirectorMovieDetailSerializers(many=True)
@@ -148,27 +165,32 @@ class MovieDetailSerializers(serializers.ModelSerializer):
     avg_rating = serializers.SerializerMethodField()
     count_user = serializers.SerializerMethodField()
     rating_connect_movie = RatingSerializers(many=True)
+
     class Meta:
         model = Movie
-        fields = ['movie_name', 'movie_image', 'director', 'actor', 'year', 'genre', 'country', 'quality', 'movie_time', 'movie_trailer',
-                  'movie_moments_connect_movie', 'description', 'movie_videos_connect_movie', 'status_movie', 'avg_rating', 'count_user',
-                  'rating_connect_movie']
+        fields = ['movie_name', 'movie_image', 'director', 'actor', 'year', 'genre', 'country',
+                  'quality', 'movie_time', 'movie_trailer', 'movie_moments_connect_movie',
+                  'description', 'movie_videos_connect_movie', 'status_movie', 'avg_rating',
+                  'count_user', 'rating_connect_movie']
 
-    def avg_rating(self, obj):
+    def get_avg_rating(self, obj):
         return obj.avg_rating()
 
-    def count_user(self, obj):
+    def get_count_user(self, obj):
         return obj.count_user()
+
 
 class SaveToFavoriteSerializers(serializers.ModelSerializer):
     class Meta:
         model = SaveToFavorite
         fields = '__all__'
 
+
 class FavoriteMoviesSerializers(serializers.ModelSerializer):
     class Meta:
         model = FavoriteMovies
         fields = ['movie', 'save_to_favorite']
+
 
 class HistorySerializers(serializers.ModelSerializer):
     class Meta:
--- movie_site/movie_app/urls.py
+++ movie_site/movie_app/urls.py
@@ -1,35 +1,32 @@
-from rest_framework import routers
-from django.urls import path, include
+from django.urls import path
 from drf_spectacular.views import SpectacularAPIView, SpectacularSwaggerView
-from .views import (UserProfileAPIView, CountryAPIView, CountryDetailAPIView, DirectorAPIView, DirectorDetailAPIView,
-                    ActorAPIView, ActorDetailAPIView, GenreAPIView, MovieAPIView, MovieDetailAPIView, FavoriteMoviesAPIView, HistoryAPIView,
+from .views import (UserProfileAPIView, CountryAPIView, CountryDetailAPIView,
+                    DirectorAPIView, DirectorDetailAPIView,
+                    ActorAPIView, ActorDetailAPIView,
+                    GenreAPIView, GenreDetailAPIView,
+                    MovieAPIView, MovieDetailAPIView,
+                    FavoriteMoviesAPIView, HistoryAPIView,
                     RegisterView, CustomLoginView, LogoutView, RatingAPIView)
 
 
-# router = routers.SimpleRouter()
-
-
 urlpatterns = [
-    # path('', include(router.urls)),
-    path('', MovieAPIView.as_view(), name = 'movies'),
-    path('<int:pk>/', MovieDetailAPIView.as_view(), name = 'movie_details'),
-    path('genre/', GenreAPIView.as_view(), name = 'genrs'),
-    path('genre/<int:pk>/', GenreAPIView.as_view(), name = 'genrs-details'),
-    path('country/', CountryAPIView.as_view(), name = 'countries'),
-    path('country/<int:pk>', CountryDetailAPIView.as_view(), name = 'countries-details'),
-    path('director/', DirectorAPIView.as_view(), name = 'directors'),
-    path('director/<int:pk>', DirectorDetailAPIView.as_view(), name = 'directors-details'),
-    path('actor/', ActorAPIView.as_view(), name = 'actors'),
-    path('actor/<int:pk>', ActorDetailAPIView.as_view(), name = 'actors-details'),
-
-    path('history/', HistoryAPIView.as_view(), name = 'histories'),
-    path('favorite_movie/', FavoriteMoviesAPIView.as_view(), name = 'favorite_movies'),
-    path('user/', UserProfileAPIView.as_view(), name = 'user'),
-    path('rating/', RatingAPIView.as_view(), name = 'ratings'),
-
-    path('register/', RegisterView.as_view(), name = 'register'),
-    path('login/', CustomLoginView.as_view(), name = 'login'),
-    path('logout/', LogoutView.as_view(), name = 'logout'),
+    path('', MovieAPIView.as_view(), name='movies'),
+    path('<int:pk>/', MovieDetailAPIView.as_view(), name='movie_details'),
+    path('genre/', GenreAPIView.as_view(), name='genres'),
+    path('genre/<int:pk>/', GenreDetailAPIView.as_view(), name='genre_details'),
+    path('country/', CountryAPIView.as_view(), name='countries'),
+    path('country/<int:pk>/', CountryDetailAPIView.as_view(), name='country_details'),
+    path('director/', DirectorAPIView.as_view(), name='directors'),
+    path('director/<int:pk>/', DirectorDetailAPIView.as_view(), name='director_details'),
+    path('actor/', ActorAPIView.as_view(), name='actors'),
+    path('actor/<int:pk>/', ActorDetailAPIView.as_view(), name='actor_details'),
+    path('history/', HistoryAPIView.as_view(), name='histories'),
+    path('favorite_movie/', FavoriteMoviesAPIView.as_view(), name='favorite_movies'),
+    path('user/', UserProfileAPIView.as_view(), name='user'),
+    path('rating/', RatingAPIView.as_view(), name='ratings'),
+    path('register/', RegisterView.as_view(), name='register'),
+    path('login/', CustomLoginView.as_view(), name='login'),
+    path('logout/', LogoutView.as_view(), name='logout'),
     path('api/schema/', SpectacularAPIView.as_view(), name='schema'),
-    path('api/docs/', SpectacularSwaggerView.as_view(url_name='schema'), name='swagger-ui')
+    path('api/docs/', SpectacularSwaggerView.as_view(url_name='schema'), name='swagger-ui'),
 ]
